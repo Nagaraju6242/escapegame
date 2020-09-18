e625644c5bcf2866ff549fe8b16662da656b8d97
Nagaraju6242 <62198428+Nagaraju6242@users.noreply.github.com>
2020-09-18 10:55:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var caller = $("#emcee"),
   box = $("#box");
 
-function is_touch_device3() {
-  return !!("ontouchstart" in window || navigator.maxTouchPoints);
-}
-
 function foo() {
   var randX = Math.floor(Math.random() * (window.innerWidth - 100));
   var randY = Math.floor(Math.random() * (window.innerHeight - 100));
@@ -13,11 +9,13 @@ function foo() {
 }
 
 $(document).ready(function () {
-  if (is_touch_device3()) {
-    alert("Please play the game without touching the screen.");
-  }
   caller.on("mouseenter", foo);
   caller.on("click", function () {
     alert("Congratulations! You Won");
   });
 });
+
+document.addEventListener("touchstart" , function(e){
+alert("Do not touch the screen");
+location.reload();
+})
